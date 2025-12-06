cced74332d00f09875c623e27c54bea207a180d0
niteesh414 <niteesh414@gmail.com>
2025-12-06 12:18:18 +0530

    removed spaces

--- bogo.js
+++ bogo.js
@@ -7,16 +7,14 @@ const prices = {
   function selectOption(num) {
     document.querySelectorAll(".option")
       .forEach(o => o.classList.remove("selected"));
-  
     document.querySelectorAll("[id^='Unit']")
       .forEach(d => (d.style.display = "none"));
-  
     document.getElementById("box" + num).classList.add("selected");
     document.getElementById("opt" + num).checked = true;
     document.getElementById("totalPrice").innerText = prices[num];
-  
     document.getElementById("Unit" + num + "Options").style.display = "block";
   }
   
   selectOption(2);
+
   
